cd47d960d3fb090c4ec53a96590b6d69c21a5dc6
AaronWright22 <70342377+imainhibana@users.noreply.github.com>
2022-04-04 13:33:31 -0500

    greedy policy

--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,32 @@
+import itertools
+import matplotlib
+import matplotlib.style
+import numpy as np
+import pandas as pd
+import sys
+
+def EpsillonGreedyPolicy(Q,epsilon,num_actions):
+
+
+    def policyFunction(state):
+        action_probabilities = np.ones(num_actions, dtype=float) * epsilon/num_actions
+        best_action = np.argmax(Q[state])
+        action_probabilities[best_action] += (1.0 -epsilon)
+        return action_probabilities
+    return policyFunction
+
+
+def agent():
+    #can move north, south, east, west
+    #can pickup something
+    #can dropoff something
+    #CANNOT OCCUPY THE SAME STATE AS ANOTHER AGENT
+    return
+
+
+
+
+
+
+
 
