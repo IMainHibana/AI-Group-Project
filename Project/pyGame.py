7200e2c60af1203dcfc599ef3f736358ddde5cc6
Chi Ngo <ctngo3@cougarnet.uh.edu>
2022-04-23 14:57:25 -0500

    changed size of display

--- Project/pyGame.py
+++ Project/pyGame.py
@@ -3,8 +3,8 @@ import sys
 
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 1000
+WINDOW_WIDTH = 1000
 
 
 def pyGameGrid():
@@ -25,7 +25,7 @@ def pyGameGrid():
 
 
 def drawGrid():
-  blockSize = 80 #Set the size of the grid block
+  blockSize = 200 #Set the size of the grid block
   for x in range(0, WINDOW_WIDTH, blockSize):
       for y in range(0, WINDOW_HEIGHT, blockSize):
           rect = pygame.Rect(x, y, blockSize, blockSize)
