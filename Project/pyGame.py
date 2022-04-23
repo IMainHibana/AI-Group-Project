5b67b0323602d9fc491163cabae21011bb51b84f
Chi Ngo <ctngo3@cougarnet.uh.edu>
2022-04-23 15:26:10 -0500

    drivers at starting point

--- Project/pyGame.py
+++ Project/pyGame.py
@@ -3,9 +3,8 @@ import sys
 
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
-WINDOW_HEIGHT = 1000
-WINDOW_WIDTH = 1000
-
+WINDOW_HEIGHT = 800
+WINDOW_WIDTH = 800
 
 def pyGameGrid():
   global SCREEN, CLOCK
@@ -25,8 +24,18 @@ def pyGameGrid():
 
 
 def drawGrid():
-  blockSize = 200 #Set the size of the grid block
+  blockSize = 160 #Set the size of the grid block
+  male = pygame.image.load('male.jpeg')
+  female = pygame.image.load('female.png')
+  IMG_SIZE = (160,160)
+  male_agent = pygame.transform.scale(male, IMG_SIZE)
+  female_agent = pygame.transform.scale(female, IMG_SIZE)
+
   for x in range(0, WINDOW_WIDTH, blockSize):
       for y in range(0, WINDOW_HEIGHT, blockSize):
           rect = pygame.Rect(x, y, blockSize, blockSize)
           pygame.draw.rect(SCREEN, BLACK, rect, 1)
+          if x == 320 and y == 0:
+              SCREEN.blit(male_agent, (x, y))
+          if x == 320 and y == 640:
+              SCREEN.blit(female_agent, (x, y))
