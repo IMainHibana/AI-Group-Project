71020b54d9f9c717d05034e36f15100f36a626b0
Chi Ngo <ctngo3@cougarnet.uh.edu>
2022-04-24 16:45:30 -0500

    updating pygame

--- Project/pyGame.py
+++ Project/pyGame.py
@@ -1,10 +1,19 @@
 import pygame
 import sys
+import numpy as np
 
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
 WINDOW_HEIGHT = 800
 WINDOW_WIDTH = 800
+ROWS = 5   #ROWS IN WORLD
+COLS = 5 #COLUMNS IN WORLD
+
+# our grid as a 2d array
+grid = np.zeros((ROWS, COLS), dtype='i,i')
+for i in range(0, ROWS):
+    for j in range(0, COLS):
+        grid[i][j] = (i+1, j+1)
 
 def pyGameGrid():
   global SCREEN, CLOCK
@@ -24,18 +33,26 @@ def pyGameGrid():
 
 
 def drawGrid():
-  blockSize = 160 #Set the size of the grid block
-  male = pygame.image.load('male.jpeg')
-  female = pygame.image.load('female.png')
-  IMG_SIZE = (160,160)
-  male_agent = pygame.transform.scale(male, IMG_SIZE)
-  female_agent = pygame.transform.scale(female, IMG_SIZE)
+    blockSize = 160 #Set the size of the grid block
+    male = pygame.image.load('male.jpeg')
+    female = pygame.image.load('female.png')
+    IMG_SIZE = (160,160)
+    male_agent = pygame.transform.scale(male, IMG_SIZE)
+    female_agent = pygame.transform.scale(female, IMG_SIZE)
+    number_font = pygame.font.SysFont(None, 22)  # Default font, Size 16
+    number_image = number_font.render("8", True, BLACK, WHITE)  # Number 8
+    row,col = 0, 0
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            number_text = str(grid[row][col])
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, BLACK, rect, 1)
+            # make the number from grid[row][col] into an image
+            number_image = number_font.render(number_text, True, BLACK, WHITE)
+            SCREEN.blit(number_image, (x,y))
+            if row == 4:
+                row = 0
+            else:
+                row += 1
+        col += 1
 
-  for x in range(0, WINDOW_WIDTH, blockSize):
-      for y in range(0, WINDOW_HEIGHT, blockSize):
-          rect = pygame.Rect(x, y, blockSize, blockSize)
-          pygame.draw.rect(SCREEN, BLACK, rect, 1)
-          if x == 320 and y == 0:
-              SCREEN.blit(male_agent, (x, y))
-          if x == 320 and y == 640:
-              SCREEN.blit(female_agent, (x, y))
