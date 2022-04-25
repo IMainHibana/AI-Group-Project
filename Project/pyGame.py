27ed10af27e6ef46618221a0a1d8d3823ba64b45
Chi Ngo <ctngo3@cougarnet.uh.edu>
2022-04-25 10:34:56 -0500

    resized

--- Project/pyGame.py
+++ Project/pyGame.py
@@ -8,6 +8,10 @@ WINDOW_HEIGHT = 800
 WINDOW_WIDTH = 800
 ROWS = 5   #ROWS IN WORLD
 COLS = 5 #COLUMNS IN WORLD
+blockSize = 160  # Set the size of the grid block
+male = pygame.image.load('male.jpeg')
+female = pygame.image.load('female.png')
+IMG_SIZE = (100, 100)
 
 # our grid as a 2d array
 grid = np.zeros((ROWS, COLS), dtype='i,i')
@@ -15,30 +19,26 @@ for i in range(0, ROWS):
     for j in range(0, COLS):
         grid[i][j] = (i+1, j+1)
 
+
 def pyGameGrid():
-  global SCREEN, CLOCK
-  pygame.init()
-  SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-  CLOCK = pygame.time.Clock()
-  SCREEN.fill(WHITE)
+    global SCREEN, CLOCK
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(WHITE)
 
-  while True:
-      drawGrid()
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              pygame.quit()
-              sys.exit()
+    while True:
+        drawGrid()
+        starting_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-      pygame.display.update()
+        pygame.display.update()
 
 
 def drawGrid():
-    blockSize = 160 #Set the size of the grid block
-    male = pygame.image.load('male.jpeg')
-    female = pygame.image.load('female.png')
-    IMG_SIZE = (160,160)
-    male_agent = pygame.transform.scale(male, IMG_SIZE)
-    female_agent = pygame.transform.scale(female, IMG_SIZE)
     number_font = pygame.font.SysFont(None, 22)  # Default font, Size 16
     number_image = number_font.render("8", True, BLACK, WHITE)  # Number 8
     row,col = 0, 0
@@ -56,3 +56,13 @@ def drawGrid():
                 row += 1
         col += 1
 
+
+def starting_pos():
+    male_agent = pygame.transform.scale(male, IMG_SIZE)
+    female_agent = pygame.transform.scale(female, IMG_SIZE)
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            if x == 320 and y == 0:
+                SCREEN.blit(male_agent, (x + 30, y + 30))
+            if x == 320 and y == 640:
+                SCREEN.blit(female_agent, (x + 30, y + 30))
