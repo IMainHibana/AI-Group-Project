c08f978ec644bd7e567247feb3d4ab3f7d0d05a4
Chi Ngo <ctngo3@cougarnet.uh.edu>
2022-05-09 02:50:54 -0500

    .

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,5 @@
 import Demo
+import numpy as np
 
 class main():
     def Run():
@@ -13,9 +14,37 @@ class main():
         agent_F = Demo.Agent(alpha, gamma)
         agent_M = Demo.Agent(alpha, gamma)
 
+        q_table = [
+            [0,0], [0,0], [0,0], [0,0], [0,0],
+            [0,0], [0,0], [0,0], [0,0], [0,0],
+            [0,0], [0,0], [0,0], [0,0], [0,0],
+            [0,0], [0,0], [0,0], [0,0], [0,0],
+            [0,0], [0,0], [0,0], [0,0], [0,0]
+        ]
 
+        reward_matrix = [
+            [13,0], [0,0], [0,0], [0,0], [13,0],
+            [0,0], [0,0], [0,0], [0,0], [0,0],
+            [0,0], [0,0], [13,0], [0,0], [13,0],
+            [0,0], [13,0], [0,0], [0,0], [0,0],
+            [0,0], [0,0], [0,0], [0,0], [13,0]
+        ]
 
-        #Exp 1:
+        pick_drop_states = [2, 4], [3, 1], [0, 0], [0, 4], [2, 2], [4, 4]
+
+        def getAllPossibleNextAction(cur_pos):
+            step_matrix = [x != None for x in reward_matrix[cur_pos]]
+            action = []
+            if (step_matrix[0]):
+                action.append(0)
+            if(step_matrix[1]):
+                action.append(1)
+            return(action)
+
+        def isGoalStateReached(cur_pos):
+            return (cur_pos in [6])
+
+        # Exp 1:
         for i in range(0, 500):
             print("=======================================new turn===================================================")
             if (cur_turn == "F"):
